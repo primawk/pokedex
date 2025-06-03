4e9de7b29eda1411450a4a9624274be51495119e
Prima <prima.kuntoro@erajaya.com>
2025-06-03 15:43:49 +0700

    add first repl logic

---
+++ dist/repl.js
@@ -0,0 +1,18 @@
+import { createInterface } from "node:readline";
+import { stdin, stdout } from "node:process";
+export function cleanInput(input) {
+    return input.split(" ").filter((item) => item.trim() !== "");
+}
+export function startREPL() {
+    const rl = createInterface({
+        input: stdin,
+        output: stdout,
+        prompt: "Pokedex > ",
+    });
+    rl.prompt();
+    rl.on("line", (input) => {
+        if (cleanInput(input).length > 0)
+            console.log(`Your command was: ${cleanInput(input)[0].toLocaleLowerCase()}`);
+        rl.prompt();
+    });
+}
--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,22 @@
+import { createInterface } from "node:readline";
+import { stdin, stdout } from "node:process";
+
 export function cleanInput(input: string): string[] {
   return input.split(" ").filter((item) => item.trim() !== "");
+}
+
+export function startREPL() {
+  const rl = createInterface({
+    input: stdin,
+    output: stdout,
+    prompt: "Pokedex > ",
+  });
+  rl.prompt();
+  rl.on("line", (input: string) => {
+    if (cleanInput(input).length > 0)
+      console.log(
+        `Your command was: ${cleanInput(input)[0].toLocaleLowerCase()}`
+      );
+    rl.prompt();
+  });
 }
